530b18395fd2f23c581cce3aadfd92aad9982a5e
AmitK <kumaramit124@gmail.com>
2023-08-01 07:06:09 +0530

    Added new project cmd wrapper

---
+++ cmd_wapper/include/cmd_fun.h
@@ -0,0 +1,13 @@
+#ifndef CMD_FUN_H
+#define CMD_FUN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define RETURN_ERR -1
+#define RETURN_OK 0
+
+int platform_model_info(char *pValue);
+
+#endif
